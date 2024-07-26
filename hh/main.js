c51587e9da2b6ac925367d13e9c5890850297d44
lvcami <163419225+lvcami@users.noreply.github.com>
2024-07-26 15:13:29 -0300

    Add files via upload

---
+++ hh/main.js
@@ -0,0 +1,7 @@
+let valorproduto=prompt("digite o valor do produto: ")
+let desconto=(valorproduto*0.7)
+if(valorproduto>=200){
+    alert("desconto=30% valor final"+desconto)
+}else{
+    alert("seu produto não possui desconto")
+}
